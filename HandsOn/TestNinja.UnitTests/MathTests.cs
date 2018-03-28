12f972265feec91ec3dba5a9d3292388fafc995b
Yves Gingras <yvesgingras@macbook-pro.gateway.2wire.net>
2018-03-28 17:16:04 -0400

    Section 2; Lecture 19 'Writing a Sumple Unit Test'.

---
+++ HandsOn/TestNinja.UnitTests/MathTests.cs
@@ -0,0 +1,19 @@
+﻿using System.Linq.Expressions;
+using NUnit.Framework;
+using TestNinja.Fundamentals;
+
+namespace TestNinja.UnitTests
+{
+    [TestFixture]
+    public class MathTests {
+
+        [Test]
+        public void Add_WhenCalled_ReturnTheSumOfArguments() {
+            var math = new Math();
+
+            var result =  math.Add(1, 2);
+            
+            Assert.That(result, Is.EqualTo(3));
+        }
+    }
+} 
--- HandsOn/TestNinja/Fundamentals/Math.cs
+++ HandsOn/TestNinja/Fundamentals/Math.cs
@@ -4,21 +4,18 @@ namespace TestNinja.Fundamentals
 {
     public class Math
     {
-        public int Add(int a, int b)
-        { 
+        public int Add(int a, int b) {
             return a + b;
         }
-        
-        public int Max(int a, int b)
-        {
+
+        public int Max(int a, int b) {
             return (a > b) ? a : b;
         }
 
-        public IEnumerable<int> GetOddNumbers(int limit)
-        {
+        public IEnumerable<int> GetOddNumbers(int limit) {
             for (var i = 0; i <= limit; i++)
                 if (i % 2 != 0)
-                    yield return i; 
+                    yield return i;
         }
     }
 }
