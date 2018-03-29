314f875be0c4ebd7265246219364c38dbebd8dfa
Yves Gingras <yvesgingras@macbook-pro.gateway.2wire.net>
2018-03-29 16:41:50 -0400

    Section 2; Lecture 20 'Black-box Testing'.

--- HandsOn/TestNinja.UnitTests/MathTests.cs
+++ HandsOn/TestNinja.UnitTests/MathTests.cs
@@ -15,5 +15,33 @@ namespace TestNinja.UnitTests
             
             Assert.That(result, Is.EqualTo(3));
         }
+
+        [Test]
+        public void Max_FirstArgumentIsGreater_ReturnTheFirstArgument() {
+            var math = new Math();
+
+            var result = math.Max(2, 1);
+            
+            Assert.That(result, Is.EqualTo(2));
+        }
+        
+        [Test]
+        public void Max_SecondArgumentIsGreater_ReturnTheSecondArgument() {
+            var math = new Math();
+
+            var result = math.Max(1, 2);
+            
+            Assert.That(result, Is.EqualTo(2));
+        }
+        
+        [Test]
+        public void Max_ArgumentAreEqual_ReturnTheSameArgument() {
+            var math = new Math();
+
+            var result = math.Max(1, 1);
+            
+            Assert.That(result, Is.EqualTo(1));
+        }
+        
     }
 } 
