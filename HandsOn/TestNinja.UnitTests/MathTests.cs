ce0486b342ce32cb34ae7e6e2ad946b2d84ed776
Yves Gingras <yvesgingras@macbook-pro.gateway.2wire.net>
2018-03-29 17:05:57 -0400

    Section 2; Lecture 21 'Set Up and Tear Down'.

--- HandsOn/TestNinja.UnitTests/MathTests.cs
+++ HandsOn/TestNinja.UnitTests/MathTests.cs
@@ -5,43 +5,44 @@ using TestNinja.Fundamentals;
 namespace TestNinja.UnitTests
 {
     [TestFixture]
-    public class MathTests {
+    public class MathTests
+    {
+        private Math _math;
 
+        [SetUp]
+        public void SetUp() {
+            _math = new Math();
+        }
+        
         [Test]
         public void Add_WhenCalled_ReturnTheSumOfArguments() {
-            var math = new Math();
 
-            var result =  math.Add(1, 2);
+            var result =  _math.Add(1, 2);
             
             Assert.That(result, Is.EqualTo(3));
         }
 
         [Test]
         public void Max_FirstArgumentIsGreater_ReturnTheFirstArgument() {
-            var math = new Math();
-
-            var result = math.Max(2, 1);
+            var result = _math.Max(2, 1);
             
             Assert.That(result, Is.EqualTo(2));
         }
         
         [Test]
         public void Max_SecondArgumentIsGreater_ReturnTheSecondArgument() {
-            var math = new Math();
 
-            var result = math.Max(1, 2);
+            var result = _math.Max(1, 2);
             
             Assert.That(result, Is.EqualTo(2));
         }
         
         [Test]
         public void Max_ArgumentAreEqual_ReturnTheSameArgument() {
-            var math = new Math();
 
-            var result = math.Max(1, 1);
+            var result = _math.Max(1, 1);
             
             Assert.That(result, Is.EqualTo(1));
         }
-        
     }
-} 
+}
