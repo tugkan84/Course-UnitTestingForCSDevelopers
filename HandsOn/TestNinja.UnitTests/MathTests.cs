e16c4bca3886466dc5c35126beeaf229176a9dd3
Yves Gingras <yvesgingras@macbook-pro.gateway.2wire.net>
2018-03-31 07:41:41 -0400

    Section 2; Lecture 22 'Parameterized Tests'.

--- HandsOn/TestNinja.UnitTests/MathTests.cs
+++ HandsOn/TestNinja.UnitTests/MathTests.cs
@@ -1,5 +1,4 @@
-﻿using System.Linq.Expressions;
-using NUnit.Framework;
+﻿using NUnit.Framework;
 using TestNinja.Fundamentals;
 
 namespace TestNinja.UnitTests
@@ -7,42 +6,28 @@ namespace TestNinja.UnitTests
     [TestFixture]
     public class MathTests
     {
-        private Math _math;
-
         [SetUp]
         public void SetUp() {
             _math = new Math();
         }
-        
+
+        private Math _math;
+
         [Test]
         public void Add_WhenCalled_ReturnTheSumOfArguments() {
+            var result = _math.Add(1, 2);
 
-            var result =  _math.Add(1, 2);
-            
             Assert.That(result, Is.EqualTo(3));
         }
 
         [Test]
-        public void Max_FirstArgumentIsGreater_ReturnTheFirstArgument() {
-            var result = _math.Max(2, 1);
-            
-            Assert.That(result, Is.EqualTo(2));
-        }
-        
-        [Test]
-        public void Max_SecondArgumentIsGreater_ReturnTheSecondArgument() {
+        [TestCase(2, 1, 2)]
+        [TestCase(1, 2, 2)]
+        [TestCase(1, 1, 1)]
+        public void Max_WhenCalled_ReturnTheGreaterArgument(int a, int b, int expectedResult) {
+            var result = _math.Max(a, b);
 
-            var result = _math.Max(1, 2);
-            
-            Assert.That(result, Is.EqualTo(2));
-        }
-        
-        [Test]
-        public void Max_ArgumentAreEqual_ReturnTheSameArgument() {
-
-            var result = _math.Max(1, 1);
-            
-            Assert.That(result, Is.EqualTo(1));
+            Assert.That(result, Is.EqualTo(expectedResult));
         }
     }
 }
