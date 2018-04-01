b4f0e08a9b61fd3d920b05abe140820c8992c90d
Yves Gingras <yvesgingras@macbook-pro.gateway.2wire.net>
2018-04-01 14:55:46 -0400

    Section 3; Lecture 30 'Testing the Return Type of Methods.'

---
+++ HandsOn/TestNinja.UnitTests/CustomerControllerTests.cs
@@ -0,0 +1,28 @@
+﻿using NUnit.Framework;
+using TestNinja.Fundamentals;
+
+namespace TestNinja.UnitTests
+{
+    [TestFixture]
+    public class CustomerControllerTests
+    {
+        [Test]
+        public void GetCustomer_IdIsZero_ReturnNotFound() {
+            var controller = new CustomerController();
+
+            var result =  controller.GetCustomer(0);
+            
+            /* The type itself */
+            Assert.That(result, Is.TypeOf<NotFound>());
+            
+            /*The type or one of its derivatives*/
+            //Assert.That(result, Is.InstanceOf<NotFound>());
+        }
+        
+        [Test]
+        public void GetCustomer_IdIsZero_ReturnOk() {
+            
+        }
+        
+    }
+}
