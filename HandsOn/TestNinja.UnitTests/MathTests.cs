be1e764f70f799fa8128777ca6fda1ea983b2837
Yves Gingras <yvesgingras@macbook-pro.gateway.2wire.net>
2018-04-01 12:48:21 -0400

    Section 3; Lecture 29 'Testing Arrays and Collections'.

--- HandsOn/TestNinja.UnitTests/MathTests.cs
+++ HandsOn/TestNinja.UnitTests/MathTests.cs
@@ -1,4 +1,5 @@
-﻿using NUnit.Framework;
+﻿using System.Linq;
+using NUnit.Framework;
 using TestNinja.Fundamentals;
 
 namespace TestNinja.UnitTests
@@ -29,6 +30,30 @@ namespace TestNinja.UnitTests
             var result = _math.Max(a, b);
 
             Assert.That(result, Is.EqualTo(expectedResult));
+        }
+
+        [Test]
+        public void GetOddNumbers_LimitIsGreaterThanZero_ReturnOddNumbersUpToLimit() {
+            var result =  _math.GetOddNumbers(5);
+
+//            Assert.That(result, Is.Not.Empty);
+            
+//            Assert.That(result.Count(), Is.EqualTo(3));
+
+            /*
+            // Running the 3 following test is equivalent to running only the forth one.
+            Assert.That(result, Does.Contain(1));
+            Assert.That(result, Does.Contain(3));
+            Assert.That(result, Does.Contain(5));
+            */
+            Assert.That(result, Is.EquivalentTo(new[] {1, 3, 5}));
+            
+            /*
+            //Other assertion that can be usefull
+            Assert.That(result, Is.Ordered);
+            Assert.That(result, Is.Unique);
+            */
+            
         }
     }
 }
