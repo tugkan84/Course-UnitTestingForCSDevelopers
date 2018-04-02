a3b8b23d9dc6ad7822559e039c27df97410db981
Yves Gingras <yvesgingras@macbook-pro.gateway.2wire.net>
2018-04-02 16:07:05 -0400

    Section 3; Lecture 34 'Testing Private Methods.'

--- HandsOn/TestNinja/Fundamentals/ErrorLogger.cs
+++ HandsOn/TestNinja/Fundamentals/ErrorLogger.cs
@@ -5,21 +5,26 @@ namespace TestNinja.Fundamentals
 {
     public class ErrorLogger
     {
+        
         public string LastError { get; set; }
 
-        public event EventHandler<Guid> ErrorLogged; 
-        
-        public void Log(string error)
-        {
-            if (String.IsNullOrWhiteSpace(error))
+        public event EventHandler<Guid> ErrorLogged;
+
+        public void Log(string error) {
+            if (string.IsNullOrWhiteSpace(error))
                 throw new ArgumentNullException();
-                
-            LastError = error; 
-            
+
+            LastError = error;
+
             // Write the log to a storage
             // ...
 
-            ErrorLogged?.Invoke(this, Guid.NewGuid());
+            OnErrorLogged(Guid.NewGuid());
+        }
+        
+        /* private method being tested through the 'Log' public method. */
+        private void OnErrorLogged(Guid errorId) {
+            ErrorLogged?.Invoke(this, errorId);
         }
     }
 }
